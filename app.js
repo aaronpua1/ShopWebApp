f0b08c9df3a80031a83cf0b03db27b45e419493f
Aaron Pua <aaron_pua@yahoo.com>
2017-05-21 00:10:15 -0700

    Enhancing user experience with colorpicker on the create offer page

--- app.js
+++ app.js
@@ -344,7 +344,7 @@ app.get('/', function(req, res) {
             });
         })
     } else {
-        console.log("THIS SOB NEEDS TO WORK: " + req);
+        console.log("THIS SOB NEEDS TO WORK: " + JSON.stringify(req.body));
         res.redirect('/install');
     }
 })
