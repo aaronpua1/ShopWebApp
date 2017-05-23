6702ebdcecacbdb92c1ac2707c446adfd59a2add
Aaron Pua <aaron_pua@yahoo.com>
2017-05-22 17:36:59 -0700

    making enhancements to upsell snippet

--- app.js
+++ app.js
@@ -53,12 +53,22 @@ app.get('/shopify_auth', function(req, res) {
 // This function initializes the Shopify OAuth Process
 // The template in views/embedded_app_redirect.ejs is rendered 
 app.get('/shopify_auth', function(req, res) {
-    res.render('embedded_app_redirect', {
-        shop: req.session.shop,
-        api_key: config.oauth.api_key,
-        scope: config.oauth.scope,
-        redirect_uri: config.oauth.redirect_uri
-    });
+    if (req.query.shop) {
+        req.session.shop = req.query.shop;
+        res.render('embedded_app_redirect', {
+            shop: req.query.shop,
+            api_key: config.oauth.api_key,
+            scope: config.oauth.scope,
+            redirect_uri: config.oauth.redirect_uri
+        });
+    } else {
+       res.render('embedded_app_redirect', {
+            shop: req.session.shop,
+            api_key: config.oauth.api_key,
+            scope: config.oauth.scope,
+            redirect_uri: config.oauth.redirect_uri
+        });
+    } 
 })
 
 // After the users clicks 'Install' on the Shopify website, they are redirected here
@@ -361,7 +371,6 @@ app.get('/', function(req, res) {
         else {
             res.redirect('/install');
         }
-
     }
 })
 
