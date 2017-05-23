3e754112b147eeb12c4f4bd29472f840b17bca26
Aaron Pua <aaron_pua@yahoo.com>
2017-05-23 00:05:56 -0700

    fixing product display issues with create offer page

--- app.js
+++ app.js
@@ -639,7 +639,7 @@ app.get('/create-offer', function(req, res) {
             return; 
         }    
         //console.log("RESULT: " + JSON.stringify(result_products));
-        result_products = JSON.parse(result_products);
+        result_products = JSON.parse(JSON.stringify(result_products));
         res.render('create_offer', {
             title: 'Create Your Offer', 
             api_key: config.oauth.api_key,
