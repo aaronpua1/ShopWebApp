0ee94fbc112439d997150fe35ab64517f5f4e7f3
Aaron Pua <aaron_pua@yahoo.com>
2017-05-26 17:39:11 -0700

    Fixing over 421 products display issues in create offer page

--- app.js
+++ app.js
@@ -628,6 +628,22 @@ app.get('/create-offer', function(req, res) {
                     console.log("PRODUCT STRING: " + JSON.stringify(store_products.products));
                 }
                 console.log("WTF");
+                  res.render('create_offer', {
+                    title: 'Create Your Offer', 
+                    api_key: config.oauth.api_key,
+                    shop: req.session.shop,
+                    product_selections: result_products,
+                    store: result_store,
+                    store_upsell: store_upsell,
+                    store_products: store_products,
+                    upsell_config: string_upsell,
+                    product_config: string_products,
+                    metafields: result_values,
+                    keys: string_keys,
+                    key: req.query.key,
+                    vendors: unique_vendors,
+                    product_type: unique_types
+                });
                 callback(null, 'done');
                 //console.log(JSON.stringify(result_metafields));
                 //console.log(JSON.stringify(result_values));
@@ -644,7 +660,7 @@ app.get('/create-offer', function(req, res) {
         //result_products = JSON.parse(JSON.stringify(result_products));
         result_products = JSON.parse(JSON.stringify(result_products));
         //console.log(util.inspect(result_products, false, null));
-        res.render('create_offer', {
+        /*res.render('create_offer', {
             title: 'Create Your Offer', 
             api_key: config.oauth.api_key,
             shop: req.session.shop,
@@ -659,7 +675,7 @@ app.get('/create-offer', function(req, res) {
             key: req.query.key,
             vendors: unique_vendors,
             product_type: unique_types
-        });
+        });*/
     });
 })
 
