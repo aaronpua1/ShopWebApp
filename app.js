31fc8bb303436eb6d08f32957a8409af773c3795
Aaron Pua <aaron_pua@yahoo.com>
2017-05-27 19:15:34 -0700

    Improving performance issues by adding metafield ids to store metafield

--- app.js
+++ app.js
@@ -1721,7 +1721,7 @@ app.post('/create-offer', function(req, res) {
         res.redirect('/');
     });    
 })
-
+/*
 //GOOD
 //https://cdn.shopify.com/s/files/1/1826/5527/products/news-icon-7728.png?v=1490957249
 //https://cdn.shopify.com/s/files/1/1826/5527/products/folder-icon-25160.png?v=1490957265
@@ -1745,7 +1745,7 @@ app.get('/delete-offer', function(req, res) {
         } 
         res.json(200);
     });
-})
+})*/
 /*
 app.get('/delete-offer', function(req, res) {
     async.waterfall([
@@ -2167,7 +2167,7 @@ app.get('/deactivate-offer', function(req, res) {
         res.json(200);
     });
 })*/
-/*
+
 app.get('/delete-offer', function(req, res) {
     async.waterfall([
         function(callback) {
@@ -2251,7 +2251,7 @@ app.get('/delete-offer', function(req, res) {
         res.json(200);
     });
 })
-*/
+
 app.get('/activate-offer', function(req, res) {
     async.waterfall([
         function(callback) {
@@ -2346,7 +2346,7 @@ app.get('/activate-offer', function(req, res) {
             
             async.map(requests, function(obj, callback) {
                 throttledRequest(obj, function(err, resp, body) {
-                    if (!err && resp.statusCode == 201) {
+                    if (!err && (resp.statusCode == 201 || resp.statusCode == 200)) {
                         var body = JSON.parse(body);
                         callback(null, body);
                     }
@@ -2468,7 +2468,7 @@ app.get('/deactivate-offer', function(req, res) {
             
             async.map(requests, function(obj, callback) {
                 throttledRequest(obj, function(err, resp, body) {
-                    if (!err && resp.statusCode == 201) {
+                    if (!err && (resp.statusCode == 201 || resp.statusCode == 200)) {
                         var body = JSON.parse(body);
                         callback(null, body);
                     }
